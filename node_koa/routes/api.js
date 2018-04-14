11ce185268a507f8087897d639f6b3713a63236d
jackdizhu <376365334@qq.com>
2018-04-14 23:08:53 +0800

    async await 理解

--- node_koa/routes/api.js
+++ node_koa/routes/api.js
@@ -41,6 +41,80 @@ router.get('/log', async (ctx, next) => {
   log(ctx.body)
 })
 
+async function fn_async1() {
+  console.log('fn_async1 执行1')
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fn_async1 执行2')
+      resolve({ fn_async1: 'fn_async1' })
+    }, 500)
+  })
+}
+async function fn_async2() {
+  console.log('fn_async2 执行1')
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('fn_async2 执行2')
+      resolve({ fn_async2: 'fn_async2' })
+    }, 500)
+  })
+}
+
+// 顺序执行
+router.get('/async', async (ctx, next) => {
+  console.log('fn_async1 开始')
+  const async1 = await fn_async1()
+  console.log(async1, 'fn_async1 结束')
+  console.log('fn_async2 开始')
+  const async2 = await fn_async2()
+  console.log(async2, 'fn_async2 结束')
+  let _obj = {
+    obj: {
+      async1: async1,
+      async2: async2
+    }
+  }
+
+  forReqFn(_obj, ctx.query, ctx.request.body)
+  ctx.body = Mock.mock(_obj)
+})
+// 并发执行 Promise.all 返回的是 Promise
+router.get('/async2', async (ctx, next) => {
+  let [async1, async2] = await Promise.all([fn_async1(), fn_async2()])
+
+  let _obj = {
+    obj: {
+      async1: async1,
+      async2: async2
+    }
+  }
+
+  forReqFn(_obj, ctx.query, ctx.request.body)
+  ctx.body = Mock.mock(_obj)
+})
+// 并发执行 await 等待结果
+router.get('/async3', async (ctx, next) => {
+  let async1 = fn_async1();
+  let async2 = fn_async2();
+
+  console.log('fn_async1 开始')
+  let _async1 = await async1;
+  console.log(async1, 'fn_async1 结束')
+  console.log('fn_async2 开始')
+  let _async2 = await async2;
+  console.log(async2, 'fn_async2 结束')
+
+  let _obj = {
+    obj: {
+      async1: _async1,
+      async2: _async2
+    }
+  }
+
+  forReqFn(_obj, ctx.query, ctx.request.body)
+  ctx.body = Mock.mock(_obj)
+})
+
 router.get('/ssr_get', async (ctx, next) => {
   let _obj = {
     'success': true,
